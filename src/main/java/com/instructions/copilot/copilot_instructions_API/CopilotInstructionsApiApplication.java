cb5915910ec563f8d2024cf3a4e6000a599e2d0f
Cristian-Steopei <cristian.steopei@tss-yonder.com>
2025-09-05 12:10:13 +0300

    fixed .env issues

--- src/main/java/com/instructions/copilot/copilot_instructions_API/CopilotInstructionsApiApplication.java
+++ src/main/java/com/instructions/copilot/copilot_instructions_API/CopilotInstructionsApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CopilotInstructionsApiApplication {
 
 	public static void main(String[] args) {
-
         SpringApplication.run(CopilotInstructionsApiApplication.class, args);
 	}
 
